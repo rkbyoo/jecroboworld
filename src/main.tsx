14b5d44db47885546aeb51186cd0a2920a149cac
rakib hussain <rakibhussain8812@gmail.com>
2025-08-21 09:20:00 +0530

    added  Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { Analytics } from '@vercel/analytics/react';
+
+createRoot(document.getElementById("root")!).render(
+	<>
+		<App />
+		<Analytics />
+	</>
+);
